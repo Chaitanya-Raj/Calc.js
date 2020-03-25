e2a8a0ac1424a558c058c571bd9bb835fa890dda
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-03-25 20:47:29 +0530

    update javascript

--- script.js
+++ script.js
@@ -5,16 +5,7 @@ const divButton = document.getElementById('div');
 const powButton = document.getElementById('pow');
 const equalButton = document.getElementById('equal');
 
-const numButton1 = document.getElementById('one');
-const numButton2 = document.getElementById('two');
-const numButton3 = document.getElementById('three');
-const numButton4 = document.getElementById('four');
-const numButton5 = document.getElementById('five');
-const numButton6 = document.getElementById('six');
-const numButton7 = document.getElementById('seven');
-const numButton8 = document.getElementById('eight');
-const numButton9 = document.getElementById('nine');
-const numButton0 = document.getElementById('zero');
+const numButtons = document.querySelectorAll('.num-keys');
 
 const clearButton = document.getElementById('c');
 const allClearButton = document.getElementById('ac');
@@ -57,48 +48,14 @@ function operate(a, op, b) {
         return power(a, b);
 }
 
-numButton0.addEventListener('click', () => {
-    display.innerHTML += numButton0.innerHTML;
-});
+function appendNum(button) {
+    display.innerHTML += button.innerHTML;
+}
 
-numButton1.addEventListener('click', () => {
-    display.innerHTML += numButton1.innerHTML;
-});
-
-numButton2.addEventListener('click', () => {
-    display.innerHTML += numButton2.innerHTML;
-});
-
-numButton3.addEventListener('click', () => {
-    display.innerHTML += numButton3.innerHTML;
-});
-
-numButton4.addEventListener('click', () => {
-    display.innerHTML += numButton4.innerHTML;
-});
-
-numButton5.addEventListener('click', () => {
-    display.innerHTML += numButton5.innerHTML;
-});
-
-numButton6.addEventListener('click', () => {
-    display.innerHTML += numButton6.innerHTML;
-});
-
-numButton7.addEventListener('click', () => {
-    display.innerHTML += numButton7.innerHTML;
-});
-
-numButton8.addEventListener('click', () => {
-    display.innerHTML += numButton8.innerHTML;
-});
-
-numButton9.addEventListener('click', () => {
-    display.innerHTML += numButton9.innerHTML;
-});
-
-clearButton.addEventListener('click', () => {
-    display.innerHTML = "";
+numButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        appendNum(button);
+    });
 });
 
 allClearButton.addEventListener('click', () => {
@@ -155,11 +112,26 @@ divButton.addEventListener('click', () => {
     display.innerHTML = "";
 });
 
+powButton.addEventListener('click', () => {
+    if (op == '') op = '+';
+    console.log(`store before = ${store}`);
+    console.log(`op = ${op}`);
+    console.log(`new entry = ${display.innerHTML}`)
+    store = operate(store, op, display.innerHTML);
+    console.log(`store after = ${store}`);
+    op = '^';
+    runningCalc.innerHTML += `${display.innerHTML}^`;
+    display.innerHTML = "";
+});
+
+
 equalButton.addEventListener('click', () => {
     console.log(`store before = ${store}`);
     console.log(`op = ${op}`);
     console.log(`new entry = ${display.innerHTML}`)
     store = operate(store, op, display.innerHTML);
     console.log(`store after = ${store}`);
+    runningCalc.innerHTML = '';
     display.innerHTML = store;
+    store = 0;
 });
